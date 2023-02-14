75943e0c951a528452f5d41902b6d5a5ec1cc6da
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-14 12:40:16 -0300

    feat: implementado função get_most_ordered_dish_per_customer no arquivo track_orders.py

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+import statistics as st
+
+
 class TrackOrders:
     def __init__(self):
         self.__consumed = list()
@@ -6,11 +9,13 @@ class TrackOrders:
         return len(self.__consumed)
 
     def add_new_order(self, customer, order, day):
-        self.__consumed.append({customer, order, day})
+        self.__consumed.append([customer, order, day])
         return self.__consumed
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return st.mode(
+            [item[1] for item in self.__consumed if item[0] == customer]
+        )
 
     def get_never_ordered_per_customer(self, customer):
         pass

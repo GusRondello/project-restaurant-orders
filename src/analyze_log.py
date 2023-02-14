7da9267ff3dedd96ce606c60f090036fe5b41723
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-14 12:09:32 -0300

    feat: implementado função analyze_log com Statistics Module no arquivo analyze_log.py

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,6 @@
 import csv
-
+import statistics as st 
+# statistics module do python https://www.w3schools.com/python/module_statistics.asp
 
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
@@ -7,6 +8,40 @@ def analyze_log(path_to_file):
     try:
         with open(path_to_file) as text:
             file = list (csv.reader(text))
-            return file
+            
+            maria_most_consumed = st.mode([
+                item[1] for item in file
+                if item[0] == "maria"
+            ])
+            
+            arnaldo_hamburguers = len([
+                item[1] for item in file
+                if item[0] == "arnaldo" and item[1] == "hamburguer"
+            ])
+            
+            items = set ([
+                item[1] for item in file
+            ])
+            
+            joao_not_consumed = items.difference(set([
+                item[1] for item in file
+                if item[0] == "joao"
+            ]))
+            
+            days = set([
+                item[2] for item in file
+            ])
+            
+            joao_not_days = days.difference(set([
+                item[2] for item in file
+                if item[0] == "arnaldo"
+            ]))
+            
+            with open("data/mkt_campaign.txt", "w") as result: 
+                result.write(f"{maria_most_consumed}\n")
+                result.write(f"{arnaldo_hamburguers}\n")
+                result.write(f"{joao_not_consumed}\n")
+                result.write(f"{joao_not_days}")
+            return result
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")

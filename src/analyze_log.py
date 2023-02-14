368a9092c64a096fdd15947dae1ad3f672e81e1d
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-14 09:56:02 -0300

    feat: implementado invalid extension error no arquivo analyze_log.py

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,6 @@
-# Primeiro Commit
+import csv
 
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")

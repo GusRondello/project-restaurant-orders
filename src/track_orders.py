46ef40b8ef571f908d3cf725d6425e5cbfddffcf
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-14 12:36:10 -0300

    feat: implementado função len no arquivo track_orders.py

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,7 +1,9 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.__consumed = list()
+
     def __len__(self):
-        pass
+        return len(self.__consumed)
 
     def add_new_order(self, customer, order, day):
         pass

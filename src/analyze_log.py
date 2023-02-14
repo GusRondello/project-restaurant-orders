3a5e89c59d1de412000e764ce288d129c115b6ca
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-14 11:45:54 -0300

    feat: implementado not found error no arquivo analyze_log.py

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -4,3 +4,9 @@ import csv
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file) as text:
+            file = list (csv.reader(text))
+            return file
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
